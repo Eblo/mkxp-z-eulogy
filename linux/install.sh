6a39f2b1812d38a909bc5514e6177b77a73c55da
Eblo <7004497+Eblo@users.noreply.github.com>
2026-08-01 18:30:31 -0400

    Build for Steamworks

--- linux/install.sh
+++ linux/install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 BINARY=$1
-ARCH=$2
-STEAM=$3
+STEAM=$2
 function get_dep()
 {
 echo "Copying $1..."
 DEP=$(ldd $BINARY | grep $1 | sed -r 's/	\w.+ => (\/.+) .+$/\1/g')
-cp "$DEP" "${MESON_INSTALL_PREFIX}/lib$ARCH"
+cp "$DEP" "${MESON_INSTALL_PREFIX}/lib64"
 }
 
-mkdir -p ${MESON_INSTALL_PREFIX}/lib$ARCH
+mkdir -p ${MESON_INSTALL_PREFIX}/lib64
 
 
 # Required by Ubuntu
@@ -21,5 +20,5 @@ get_dep libbsd
 
 if [ -n "$STEAM" ]; then
   echo "Copying steam_api..."
-  cp "$STEAM/libsteam_api.so" "${MESON_INSTALL_PREFIX}/usr/lib"
+  cp "$STEAM/libsteam_api.so" "${MESON_INSTALL_PREFIX}"
 fi
