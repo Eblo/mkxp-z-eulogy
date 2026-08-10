56017b04850b029bdad6ebf736b433cf9344fb5f
Eblo <7004497+Eblo@users.noreply.github.com>
2026-08-10 15:18:14 -0400

    Use mkxp-z.sh for macOS as well

--- mkxp-z.sh
+++ mkxp-z.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 GAMEROOT=$(cd "${0%/*}" && echo $PWD)
-export LD_LIBRARY_PATH="${GAMEROOT}/lib64":$LD_LIBRARY_PATH
-"${GAMEROOT}"/mkxp-z "$@"
+UNAME=`uname`
+if [ "$UNAME" == "Darwin" ]; then
+	# macOS
+	LINK_DIR="${GAMEROOT}/Z-steam.app/Contents/Game"
+	if [ ! -e "$LINK_DIR" ] && [ ! -L "$LINK_DIR" ]; then
+		ln -s "$GAMEROOT" "$LINK_DIR"
+	fi
+	exec ${GAMEROOT}/Z-steam.app/Contents/MacOS/shim
+else
+	# Linux
+	export LD_LIBRARY_PATH="${GAMEROOT}/lib64":$LD_LIBRARY_PATH
+	"${GAMEROOT}"/mkxp-z "$@"
+fi
